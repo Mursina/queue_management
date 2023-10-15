929983512191768d2ac83b57954af051f2e2f678
Mursina <fathimamursina2015@gmail.com>
2023-10-15 20:22:22 +0530

    Create ORM model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Token(models.Model):
+    token = models.AutoField(primary_key=True)
+    counter = models.ForeignKey('Counter', on_delete=models.CASCADE)
+    service = models.ForeignKey('Service', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Service(models.Model):
+    service_id = models.AutoField(primary_key=True)
+    serivice_name = models.CharField(max_length=255)
+
+
+class Counter(models.Model):
+    counter_id = models.AutoField(primary_key=True)
+    status = models.CharField(max_length=255)
+
+
+class System(models.Model):
+    number_of_counters = models.PositiveIntegerField()
+    services = models.JSONField()
+    service_per_counter = models.JSONField()
+    counter_status = models.JSONField()
+    token_limit_per_counter = models.PositiveBigIntegerField()
+    service_modes = models.JSONField()
