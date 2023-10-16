37ee1e208f5248b37750c5eb60b6b4508d1c6d33
Mursina <fathimamursina2015@gmail.com>
2023-10-16 14:01:14 +0530

    Register token table

--- home/admin.py
+++ home/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'counter', 'service', 'created')
+
 class CounterAdmin(admin.ModelAdmin):
     list_display = ('counter_id', 'service_id', 'status')
 
@@ -10,3 +13,4 @@ class ServiceAdmin(admin.ModelAdmin):
 
 admin.site.register(models.Counter, CounterAdmin)
 admin.site.register(models.Service, ServiceAdmin)
+admin.site.register(models.Token, TokenAdmin)
